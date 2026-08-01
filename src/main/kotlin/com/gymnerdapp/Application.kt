bfacf7763ec28ac00ea0a2cba097494226bf39e6
Luan Barbosa <luanbarbosagomes@gmail.com>
2026-08-01 09:51:40 -0300

    Add ETAG pattern to /catalogExercise

--- src/main/kotlin/com/gymnerdapp/Application.kt
+++ src/main/kotlin/com/gymnerdapp/Application.kt
@@ -1,22 +1,31 @@
 package com.gymnerdapp
 
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.header
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 import java.io.File
+import java.security.MessageDigest
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
+private fun sha256Hex(input: String): String {
+    val digest = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+    return digest.joinToString("") { "%02x".format(it) }
+}
+
 fun Application.module() {
-    val exercises = Json.decodeFromString<List<Exercise>>(
-        File("data/exercises.json").readText()
-    )
+    val exercisesJson = File("data/exercises.json").readText()
+    val exercises = Json.decodeFromString<List<Exercise>>(exercisesJson)
+    val catalogEtag = "\"" + sha256Hex(exercisesJson) + "\""
 
     install(ContentNegotiation) {
         json()
@@ -27,6 +36,11 @@ fun Application.module() {
             call.respondText("GymNerdAppServer running")
         }
         get("/catalogExercise") {
+            call.response.header(HttpHeaders.ETag, catalogEtag)
+            if (call.request.header(HttpHeaders.IfNoneMatch) == catalogEtag) {
+                call.respond(HttpStatusCode.NotModified)
+                return@get
+            }
             call.respond(exercises)
         }
     }
